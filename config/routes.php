546c4f4b040c72f1b76c45429d6e1cd56e583094
lupeng <lupeng@hoge.cn>
2020-09-23 09:22:18 +0800

    fix routes

--- config/routes.php
+++ config/routes.php
@@ -7,12 +7,6 @@ declare(strict_types=1);
 
 return [
     'http' => [
-        // Tools
-        ['POST', 'sendVerifyCode/{mobile:1[3456789]\d{9}}', 'ToolsController@sendVerifyCode'],
-
-        //Auth
-        ['GET', 'login', 'Auth\LoginController@login'],
-
         ['ANY', '/', 'IndexController@index'],
         'group' => [
             ['GET', '/index', 'IndexController@index']
