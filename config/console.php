8c675a670634c861476deed774f04b82fe302b46
lupeng <lupeng@hoge.cn>
2020-10-09 16:49:36 +0800

    update

--- config/console.php
+++ config/console.php
@@ -12,4 +12,5 @@ return [
     \Mini\Command\HelloMiniCommandService::class,
     \Mini\Command\RouteCommandService::class,
     \Mini\Command\TestCommandService::class,
+    \Mini\Command\LogStatusCommandService::class,
 ];
