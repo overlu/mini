03a23cb22327cabf5b01de23b81414320e0433b7
lupeng <lupeng@admin.com>
2020-12-20 19:07:49 +0800

    change resource path

---
+++ resources/lang/zh_CN/attribute.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of Mini.
+ * @auth lupeng
+ */
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'name' => '名称',
+    'username' => '用户名',
+    'email' => '邮箱',
+    'first_name' => '名',
+    'last_name' => '姓',
+    'password' => '密码',
+    'password_confirmation' => '确认密码',
+    'city' => '城市',
+    'country' => '国家',
+    'address' => '地址',
+    'phone' => '电话',
+    'mobile' => '手机',
+    'age' => '年龄',
+    'sex' => '性别',
+    'gender' => '性别',
+    'day' => '天',
+    'month' => '月',
+    'year' => '年',
+    'hour' => '时',
+    'minute' => '分',
+    'second' => '秒',
+    'title' => '标题',
+    'content' => '内容',
+    'description' => '描述',
+    'excerpt' => '摘要',
+    'date' => '日期',
+    'time' => '时间',
+    'available' => '可用的',
+    'size' => '大小',
+];
