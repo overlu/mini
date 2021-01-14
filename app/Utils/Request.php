886c9c488e034ca8764d39d61aed70b3b49ca834
lu <lu@peng.com>
2021-01-14 18:01:49 +0800

    update request util

--- app/Utils/Request.php
+++ app/Utils/Request.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace App\Utils;
 
+use RuntimeException;
 use Swoole\Coroutine\Http\Client;
 
 /**
@@ -27,8 +28,8 @@ class Request
      */
     public static function post(string $url, $data = [], array $headers = [])
     {
-        $scheme = parse_url($url);
-        $client = new Client($scheme['host'], $scheme['port'] ?? 80, ($scheme['scheme'] ?? '') === 'https');
+        $scheme = self::praseUrl($url);
+        $client = new Client($scheme['host'], $scheme['port'], $scheme['ssl']);
         $client->setHeaders($headers);
         $client->set(['timeout' => 5]);
         $client->post($scheme['path'] ?? '/', $data);
@@ -46,12 +47,10 @@ class Request
      */
     public static function get(string $url, array $data = [], array $headers = [])
     {
-        $scheme = parse_url($url);
-        $client = new Client($scheme['host'], $scheme['port'] ?? 80, ($scheme['scheme'] ?? '') === 'https');
+        $scheme = self::praseUrl($url);
+        $client = new Client($scheme['host'], $scheme['port'], $scheme['ssl']);
         $client->setHeaders($headers);
         $client->set(['timeout' => 5]);
-//        $client->setData($data);
-//        $client->get($scheme['path'] ?? '/');
         $client->get(($scheme['path'] ?? '/') . '?' . http_build_query($data));
         $content = $client->getBody();
         $client->close();
@@ -66,8 +65,8 @@ class Request
      */
     public static function download(string $url, string $saveFile)
     {
-        $scheme = parse_url($url);
-        $client = new Client($scheme['host'], $scheme['port'] ?? 80, ($scheme['scheme'] ?? '') === 'https');
+        $scheme = self::praseUrl($url);
+        $client = new Client($scheme['host'], $scheme['port'], $scheme['ssl']);
         $client->set(['timeout' => -1]);
         $result = $client->download($scheme['path'] ?? '/', $saveFile);
         $client->close();
@@ -89,19 +88,37 @@ class Request
      * @param string $url
      * @param array $data
      * @param array $headers
+     * @param array $cookies
      * @return mixed
      */
-    private static function execute(string $method, string $url, array $data = [], array $headers = [])
+    public static function execute(string $method, string $url, array $data = [], array $headers = [], array $cookies = [])
     {
-        $scheme = parse_url($url);
-        $client = new Client($scheme['host'], $scheme['port'] ?? 80, ($scheme['scheme'] ?? '') === 'https');
+        $scheme = self::praseUrl($url);
+        $client = new Client($scheme['host'], $scheme['port'], $scheme['ssl']);
         $client->setHeaders($headers);
         $client->set(['timeout' => 5]);
         $client->setData($data);
+        $client->setCookies($cookies);
         $client->setMethod(strtoupper($method));
         $client->execute($scheme['path'] ?? '/');
         $content = $client->getBody();
         $client->close();
         return is_json($content) ? json_decode($content, true) : $content;
+    }
+
+    /**
+     * @param string $url
+     * @return array|false|int|string|null
+     */
+    public static function praseUrl(string $url)
+    {
+        $scheme = parse_url($url);
+        if (!isset($scheme['host'])) {
+            throw new RuntimeException('Illegal url format: ' . $url);
+        }
+        $scheme['scheme'] = $scheme['scheme'] ?? 'http';
+        $scheme['port'] = $scheme['port'] ?? 80;
+        $scheme['ssl'] = strtolower($scheme['scheme']) === 'https';
+        return $scheme;
     }
 }
