0101ff61c08b09c6a40b76c66a39be07d42bc5da
lu <lu@peng.com>
2021-01-14 11:31:01 +0800

    add crontab config

---
+++ config/crontab.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Mini.
+ * @auth lupeng
+ */
+
+declare(strict_types=1);
+
+return [
+    /**
+     * enable crontab task
+     */
+    'enable_crontab' => env('ENABLE_CRONTAB', false),
+
+    /**
+     *
+     */
+    'enable_crontab_log' => env('ENABLE_CRONTAB_LOG', false),
+
+    /**
+     * enable crontab coroutine
+     */
+    'enable_crontab_coroutine' => true,
+
+    /**
+     * crontab task list
+     * \Mini\Crontab\CrontabTaskInterface[]
+     */
+    'crontab_task_list' => []
+];
