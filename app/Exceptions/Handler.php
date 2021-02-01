f195f8092c85609f8706e957d67e0e07298c8958
lu <lu@peng.com>
2021-02-01 14:10:43 +0800

    update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,18 +8,28 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Mini\Contracts\HttpMessage\RequestInterface;
-use \Mini\Exception\Handler as MiniHandler;
+use Mini\Exception\Handler as MiniHandler;
 use Throwable;
 
 class Handler extends MiniHandler
 {
+    /**
+     * @var array
+     */
     protected array $dontReport = [];
 
+    /**
+     * @param Throwable $throwable
+     */
     public function report(Throwable $throwable): void
     {
         parent::report($throwable);
     }
 
+    /**
+     * @param RequestInterface $request
+     * @param Throwable $throwable
+     */
     public function render(RequestInterface $request, Throwable $throwable): void
     {
         parent::render($request, $throwable);
