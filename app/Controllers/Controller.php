f852be702e0744be78337b99b5ae3f680f4f7208
lu <lu@peng.com>
2021-03-17 16:00:56 +0800

    fix remove controller mixin

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -7,15 +7,11 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use Exception;
 use Mini\Contracts\HttpMessage\ControllerInterface;
-use Mini\Service\HttpMessage\Server\Response;
-use function response;
 
 /**
  * Class Controller
  * @package App\Controller
- * @mixin \Mini\Service\HttpServer\Response | Response
  */
 class Controller implements ControllerInterface
 {
@@ -64,16 +60,5 @@ class Controller implements ControllerInterface
     public function afterDispatch($response)
     {
         return $response;
-    }
-
-    /**
-     * @param $name
-     * @param $arguments
-     * @return mixed
-     * @throws Exception
-     */
-    public function __call($name, $arguments)
-    {
-        return response()->$name(...$arguments);
     }
 }
