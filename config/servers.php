3c2fe7adc7eedb75e64fd1242cfdf225e9b3061e
lupeng <lupeng.me@gmail.com>
2024-07-31 20:24:03 +0800

    update

--- config/servers.php
+++ config/servers.php
@@ -12,7 +12,7 @@ use App\Events\MqttServer;
 return [
     'http' => [
         'ip' => '0.0.0.0',
-        'port' => 9501,
+        'port' => env('HTTP_SERVER_PORT', 9501),
         'sock_type' => SWOOLE_SOCK_TCP,
         'callbacks' => [
         ],
@@ -20,16 +20,16 @@ return [
             /**
              * @url https://wiki.swoole.com/#/server/setting
              */
-            'reactor_num' => swoole_cpu_num() * 2,
-            'worker_num' => swoole_cpu_num() * 2,
+            'reactor_num' => is_dev_env(true) ? 2 : swoole_cpu_num() * 2,
+            'worker_num' => is_dev_env(true) ? 2 : swoole_cpu_num() * 2,
             'pid_file' => runtime_path('http.server.pid'),
             'log_file' => runtime_path('logs/mini.http.log'),
             'log_level' => SWOOLE_LOG_ERROR,
 //            'max_request' => 10000,   // 设置 worker 进程的最大任务数， 进程退出后会释放所有内存和资源
-            'task_worker_num' => swoole_cpu_num(),   // 配置 Task 进程的数量。【默认值：未配置则不启动 task】
+            'task_worker_num' => is_dev_env(true) ? 2 : swoole_cpu_num(),   // 配置 Task 进程的数量。【默认值：未配置则不启动 task】
             'task_enable_coroutine' => true,  // 开启 Task 协程支持
             'task_use_object' => true,    // 设置为 true 时，onTask 回调将变成对象模式
-//            'buffer_output_size' => 6 * 1024 * 1024,    // 单次 Server->send 最大允许发送 6M 字节的数据，默认 2M
+            'buffer_output_size' => 2 * 1024 * 1024,    // 单次 Server->send 最大允许发送 6M 字节的数据，默认 2M
 //            'daemonize' => true,      // 守护进程化，使用 systemd 或者 supervisord 管理 Swoole 服务时，请勿设置 daemonize = 1
 //            'chroot' => BASE_PATH,    // 重定向 Worker 进程的文件系统根目录
 //            'user' => 'Apache'        // 设置 Worker/TaskWorker 子进程的所属用户。【默认值：执行脚本用户】
@@ -38,6 +38,7 @@ return [
 //            'ssl_key_file' => BASE_PATH.'/storage/app/ssl.key',
             /******http config******/
             'upload_tmp_dir' => runtime_path('tmp'),
+            'upload_max_filesize' => 100 * 1024 * 1024,
 //            'enable_static_handler' => true,
 //            'http_autoindex' => true,
 //            'document_root' => BASE_PATH,
@@ -49,7 +50,7 @@ return [
     ],
     'ws' => [
         'ip' => '0.0.0.0',
-        'port' => 9502,
+        'port' => env('WS_SERVER_PORT', 9502),
         'sock_type' => SWOOLE_SOCK_TCP,
         'callbacks' => [
             "open" => [WebSocket::class, 'onOpen'],
@@ -73,7 +74,7 @@ return [
     ],
     'wshttp' => [
         'ip' => '0.0.0.0',
-        'port' => 9503,
+        'port' => env('WSHTTP_SERVER_PORT', 9503),
         'sock_type' => SWOOLE_SOCK_TCP,
         'callbacks' => [
 //            "open" => [WebSocket::class, 'onOpen'],
@@ -92,14 +93,16 @@ return [
             'task_use_object' => true,    // 设置为 true 时，onTask 回调将变成对象模式
             'upload_tmp_dir' => runtime_path('tmp'),
             'open_http2_protocol' => true,
+            'upload_max_filesize' => 100 * 1024 * 1024,
             'heartbeat_idle_time' => 600,   // 表示一个连接如果600秒内未向服务器发送任何数据，此连接将被强制关闭
             'heartbeat_check_interval' => 60, // 表示每60秒遍历一次，默认为false
+            'max_coroutine' => 20000
         ],
         'mode' => SWOOLE_PROCESS,
     ],
     /*'tcp' => [
         'ip' => '0.0.0.0',
-        'port' => 9504,
+        'port' => env('TCP_SERVER_PORT', 9504),
         'sock_type' => SWOOLE_SOCK_TCP,
         'callbacks' => [
             "receive" => function($server, $data){
@@ -115,7 +118,7 @@ return [
     ],*/
     /*'mqtt' => [
         'ip' => '0.0.0.0',
-        'port' => 9505,
+        'port' => env('MQTT_SERVER_PORT', 9505),
         'callbacks' => [
         ],
         'receiveCallbacks' => [
